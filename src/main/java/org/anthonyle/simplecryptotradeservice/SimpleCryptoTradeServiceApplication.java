9c69f460775c6aae794a39e4f364d4385edc65b6
Phat.Le <phat.le@sevencircles.com>
2024-06-29 12:47:31 +0200

    feat: [master] initial commit

---
+++ src/main/java/org/anthonyle/simplecryptotradeservice/SimpleCryptoTradeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.anthonyle.simplecryptotradeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCryptoTradeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCryptoTradeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/anthonyle/simplecryptotradeservice/SimpleCryptoTradeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anthonyle.simplecryptotradeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCryptoTradeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
